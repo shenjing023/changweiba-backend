c41cda6e2bafbb08d77dc3e5559bcc3d66edb032
liuwei <lw137885@126.com>
2019-11-05 16:25:42 +0800

    update

--- rpc/post/service.go
+++ rpc/post/service.go
@@ -1,5 +1,4 @@
-//go:generate protoc  --plugin=protoc-gen-micro=$GOPATH/bin/protoc-gen-micro --micro_out=.
-// /pb --go_out=plugins=grpc:./pb post.proto
+//go:generate protoc  --plugin=protoc-gen-micro=C:\GoProjects/bin/protoc-gen-micro.exe --micro_out=./pb --go_out=plugins=grpc:./pb post.proto
 
 package post
 
@@ -28,13 +27,13 @@ func (p *Post) NewPost(ctx context.Context,pr *pb.NewPostRequest) (*pb.NewPostRe
 	if len(strings.TrimSpace(pr.Topic))==0 || len(strings.TrimSpace(pr.Content))==0{
 		return nil,status.Error(codes.InvalidArgument,"topic or content can not be empty")
 	}
-	postId,err:=dao.InsertPost(int(pr.UserId),pr.Topic,pr.Content)
+	postId,err:=dao.InsertPost(pr.UserId,pr.Topic,pr.Content)
 	if err!=nil{
 		logs.Error("insert post error: ",err.Error())
 		return nil,status.Error(codes.Internal,sysError)
 	}
 	return &pb.NewPostResponse{
-		PostId:postId,
+		PostId:int64(postId),
 	},nil
 }
 
@@ -48,7 +47,7 @@ func (p *Post) NewComment(ctx context.Context,cr *pb.NewCommentRequest) (*pb.New
 		return nil,status.Error(codes.Internal,sysError)
 	}
 	return &pb.NewCommentResponse{
-		CommentId:commentId,
+		CommentId:int64(commentId),
 	}, nil
 }
 
@@ -100,98 +99,69 @@ func (p *Post) DeleteReply(ctx context.Context,dr *pb.DeleteRequest) (*pb.Delete
 }
 
 func (p *Post) GetPost(ctx context.Context,pr *pb.PostRequest) (*pb.PostResponse,error){
-	dbPost:=&dao.Post{
-		Id:pr.Id,
-	}
-	has,err:=dao.GetPost(dbPost)
+	dbPost,err:=dao.GetPost(pr.Id)
 	if err!=nil{
 		logs.Error("get post error:",err.Error())
 		return nil,status.Error(codes.Internal,sysError)
 	}
-	if has{
-		pbPost:=&pb.PostResponse{
-			Post:&pb.Post{
-				Id:dbPost.Id,
-				UserId:dbPost.UserId,
-				Topic:dbPost.Topic,
-				CreateTime:dbPost.CreateTime,
-				LastUpdate:dbPost.LastUpdate,
-				ReplyNum:dbPost.ReplyNum,
-				Status:pb.Status(dbPost.Status),
-			},
-		}
-		return pbPost,nil
-	} else{
-		logs.Info("get post failed: post is not exist")
-		return &pb.PostResponse{}, nil
+	pbPost:=&pb.PostResponse{
+		Post:&pb.Post{
+			Id:dbPost.Id,
+			UserId:dbPost.UserId,
+			Topic:dbPost.Topic,
+			CreateTime:dbPost.CreateTime,
+			LastUpdate:dbPost.LastUpdate,
+			ReplyNum:dbPost.ReplyNum,
+			Status:pb.Status(dbPost.Status),
+		},
 	}
+	return pbPost,nil
 }
 
 func (p *Post) GetComment(ctx context.Context,cr *pb.CommentRequest) (*pb.CommentResponse,error){
-	dbComment:=&dao.Comment{
-		Id:cr.Id,
-	}
-	has,err:=dao.GetComment(dbComment)
+	dbComment,err:=dao.GetComment(cr.Id)
 	if err!=nil{
 		logs.Error("get comment error:",err.Error())
 		return nil,status.Error(codes.Internal,sysError)
 	}
-	if has{
-		pbComment:=&pb.CommentResponse{
-			Comment:&pb.Comment{
-				Id:dbComment.Id,
-				UserId:dbComment.UserId,
-				PostId:dbComment.PostId,
-				Content:dbComment.Content,
-				CreateTime:dbComment.CreateTime,
-				Floor:dbComment.Floor,
-				Status:pb.Status(dbComment.Status),
-			},
-		}
-		return pbComment,nil
-	} else{
-		logs.Info("get comment failed: comment is not exist")
-		return &pb.CommentResponse{}, nil
+	pbComment:=&pb.CommentResponse{
+		Comment:&pb.Comment{
+			Id:dbComment.Id,
+			UserId:dbComment.UserId,
+			PostId:dbComment.PostId,
+			Content:dbComment.Content,
+			CreateTime:dbComment.CreateTime,
+			Floor:dbComment.Floor,
+			Status:pb.Status(dbComment.Status),
+		},
 	}
+	return pbComment,nil
 }
 
 func (p *Post) GetReply(ctx context.Context,rr *pb.ReplyRequest) (*pb.ReplyResponse,error){
-	dbReply:=&dao.Reply{
-		Id:rr.Id,
-	}
-	has,err:=dao.GetReply(dbReply)
+	dbReply,err:=dao.GetReply(rr.Id)
 	if err!=nil{
 		logs.Error("get reply error:",err.Error())
 		return nil,status.Error(codes.Internal,sysError)
 	}
-	if has{
-		pbReply:=&pb.ReplyResponse{
-			Reply:&pb.Reply{
-				Id:dbReply.Id,
-				UserId:dbReply.UserId,
-				PostId:dbReply.PostId,
-				CommentId:dbReply.CommentId,
-				Content:dbReply.Content,
-				CreateTime:dbReply.CreateTime,
-				ParentId:dbReply.ParentId,
-				Floor:int32(dbReply.Floor),
-				Status:pb.Status(dbReply.Status),
-			},
-		}
-		return pbReply,nil
-	} else{
-		logs.Info("get reply failed: reply is not exist")
-		return &pb.ReplyResponse{}, nil
+	pbReply:=&pb.ReplyResponse{
+		Reply:&pb.Reply{
+			Id:dbReply.Id,
+			UserId:dbReply.UserId,
+			PostId:dbReply.PostId,
+			CommentId:dbReply.CommentId,
+			Content:dbReply.Content,
+			CreateTime:dbReply.CreateTime,
+			ParentId:dbReply.ParentId,
+			Floor:dbReply.Floor,
+			Status:pb.Status(dbReply.Status),
+		},
 	}
+	return pbReply,nil
 }
 
 func (p *Post) GetCommentsByPostId(ctx context.Context,cr *pb.CommentsRequest) (*pb.CommentsResponse,error){
-	totalCount,err:=dao.GetCommentsCountByPostId(cr.PostId)
-	if err!=nil{
-		logs.Error(fmt.Sprintf("get comments_count by post_id failed: %+v",err))
-		return nil, status.Error(codes.Internal,sysError)
-	}
-	dbComments,err:=dao.GetCommentsByPostId(cr.PostId,int(cr.Offset),int(cr.Limit))
+	dbComments,totalCount,err:=dao.GetCommentsByPostId(cr.PostId,cr.Page,cr.PageSize)
 	if err!=nil{
 		logs.Error(fmt.Sprintf("get comments by post_id failed: %+v",err))
 		return nil, status.Error(codes.Internal,sysError)
@@ -209,18 +179,13 @@ func (p *Post) GetCommentsByPostId(ctx context.Context,cr *pb.CommentsRequest) (
 		})
 	}
 	return &pb.CommentsResponse{
-		Comments:             comments,
-		TotalCount:int32(totalCount),
+		Comments: comments,
+		TotalCount: totalCount,
 	},nil
 }
 
 func (p *Post) GetRepliesByCommentId(ctx context.Context,rr *pb.RepliesRequest) (*pb.RepliesResponse,error){
-	totalCount,err:=dao.GetRepliesCountByCommentId(rr.CommentId)
-	if err!=nil{
-		logs.Error(fmt.Sprintf("get replies_count by comment_id failed: %+v",err))
-		return nil,status.Error(codes.Internal,sysError)
-	}
-	dbReplies,err:=dao.GetRepliesByCommentId(rr.CommentId,int(rr.Offset),int(rr.Limit))
+	dbReplies,totalCount,err:=dao.GetRepliesByCommentId(rr.CommentId,rr.Page,rr.PageSize)
 	if err!=nil{
 		logs.Error(fmt.Sprintf("get replies by comment_id failed: %+v",err))
 		return nil,status.Error(codes.Internal,sysError)
@@ -241,17 +206,12 @@ func (p *Post) GetRepliesByCommentId(ctx context.Context,rr *pb.RepliesRequest) 
 	}
 	return &pb.RepliesResponse{
 		Replies:replies,
-		TotalCount:int32(totalCount),
+		TotalCount:totalCount,
 	}, nil
 }
 
 func (p *Post) Posts(ctx context.Context,pr *pb.PostsRequest) (*pb.PostsResponse,error){
-	totalCount,err:=dao.GetPostsCount()
-	if err!=nil{
-		logs.Error(fmt.Sprintf("get posts_count failed: %+v",err))
-		return nil,status.Error(codes.Internal,sysError)
-	}
-	dbPosts,err:=dao.GetPosts(int(pr.Offset),int(pr.Limit))
+	dbPosts,totalCount,err:=dao.GetPosts(pr.Page,pr.PageSize)
 	if err!=nil{
 		logs.Error(fmt.Sprintf("get posts failed: %+v",err))
 		return nil,status.Error(codes.Internal,sysError)
@@ -271,48 +231,43 @@ func (p *Post) Posts(ctx context.Context,pr *pb.PostsRequest) (*pb.PostsResponse
 	}
 	return &pb.PostsResponse{
 		Posts:posts,
-		TotalCount:int32(totalCount),
+		TotalCount:totalCount,
 	}, nil
 }
 
-func (p *Post) GetRepliesByCommentIds(ctx context.Context,rr *pb.RepliesByCommentsRequest) (*pb.
-	RepliesByCommentsResponse,error){
-	dbReplies,err:=dao.GetRepliesByCommentIds(rr.CommentIds,int(rr.Limit))
-	if err!=nil{
-		logs.Error(fmt.Sprintf("get replies by comment_id failed: %+v",err))
-		return nil,err
-	}
-	var replies []*pb.RepliesByCommentsResponse_Replies_
-	for _,v:=range dbReplies{
-		var temp []*pb.Reply
-		for _,vv:=range v{
-			temp=append(temp,&pb.Reply{
-				Id:vv.Id,
-				UserId:vv.UserId,
-				PostId:vv.PostId,
-				CommentId:vv.CommentId,
-				Content:vv.Content,
-				CreateTime:vv.CreateTime,
-				ParentId:vv.ParentId,
-				Floor:vv.Floor,
-			})
-		}
-		replies=append(replies,&pb.RepliesByCommentsResponse_Replies_{
-			Replies_:temp,
-		})
-	}
-	return &pb.RepliesByCommentsResponse{
-		Replies:replies,
-	},nil
-}
+//func (p *Post) GetRepliesByCommentIds(ctx context.Context,rr *pb.RepliesByCommentsRequest) (*pb.
+//	RepliesByCommentsResponse,error){
+//	dbReplies,err:=dao.GetRepliesByCommentIds(rr.CommentIds,int(rr.Limit))
+//	if err!=nil{
+//		logs.Error(fmt.Sprintf("get replies by comment_id failed: %+v",err))
+//		return nil,err
+//	}
+//	var replies []*pb.RepliesByCommentsResponse_Replies_
+//	for _,v:=range dbReplies{
+//		var temp []*pb.Reply
+//		for _,vv:=range v{
+//			temp=append(temp,&pb.Reply{
+//				Id:vv.Id,
+//				UserId:vv.UserId,
+//				PostId:vv.PostId,
+//				CommentId:vv.CommentId,
+//				Content:vv.Content,
+//				CreateTime:vv.CreateTime,
+//				ParentId:vv.ParentId,
+//				Floor:vv.Floor,
+//			})
+//		}
+//		replies=append(replies,&pb.RepliesByCommentsResponse_Replies_{
+//			Replies_:temp,
+//		})
+//	}
+//	return &pb.RepliesByCommentsResponse{
+//		Replies:replies,
+//	},nil
+//}
 
 func (p *Post) GetPostsByUserId(ctx context.Context, pr *pb.PostsByUserIdRequest) (*pb.PostsByUserIdResponse, error){
-	totalCount,err:=dao.GetPostsCountByUserId(pr.UserId)
-	if err!=nil{
-		logs.Error(fmt.Sprintf("get posts_count by user_id failed: %+v",err))
-		return nil,status.Error(codes.Internal,sysError)
-	}
-	dbPosts,err:=dao.GetPostsByUserId(pr.UserId,int(pr.Offset),int(pr.Limit))
+	dbPosts,totalCount,err:=dao.GetPostsByUserId(pr.UserId,pr.Page,pr.PageSize)
 	if err!=nil{
 		logs.Error(fmt.Sprintf("get posts by user_id failed: %+v",err))
 		return nil,err
@@ -331,18 +286,12 @@ func (p *Post) GetPostsByUserId(ctx context.Context, pr *pb.PostsByUserIdRequest
 	}
 	return &pb.PostsByUserIdResponse{
 		Posts:posts,
-		TotalCount:int32(totalCount),
+		TotalCount:totalCount,
 	}, nil
 }
 
-func (p *Post) GetCommentsByUserId(ctx context.Context, cr *pb.CommentsByUserIdRequest) (*pb.CommentsByUserIdResponse, 
-	error){
-	totalCount,err:=dao.GetCommentsCountByUserId(cr.UserId)
-	if err!=nil{
-		logs.Error(fmt.Sprintf("get comments_count by user_id failed: %+v",err))
-		return nil,status.Error(codes.Internal,sysError)
-	}
-	dbComments,err:=dao.GetCommentsByUserId(cr.UserId,int(cr.Offset),int(cr.Limit))
+func (p *Post) GetCommentsByUserId(ctx context.Context, cr *pb.CommentsByUserIdRequest) (*pb.CommentsByUserIdResponse, error){
+	dbComments,totalCount,err:=dao.GetCommentsByUserId(cr.UserId,cr.Page,cr.PageSize)
 	if err!=nil{
 		logs.Error(fmt.Sprintf("get comments by user_id failed: %+v",err))
 		return nil,err
@@ -361,17 +310,12 @@ func (p *Post) GetCommentsByUserId(ctx context.Context, cr *pb.CommentsByUserIdR
 	}
 	return &pb.CommentsByUserIdResponse{
 		Comments:comments,
-		TotalCount:int32(totalCount),
+		TotalCount:totalCount,
 	}, nil
 }
 
 func (p *Post) GetRepliesByUserId(ctx context.Context, rr *pb.RepliesByUserIdRequest) (*pb.RepliesByUserIdResponse, error){
-	totalCount,err:=dao.GetRepliesCountByUserId(rr.UserId)
-	if err!=nil{
-		logs.Error(fmt.Sprintf("get replies_count by user_id failed: %+v",err))
-		return nil,status.Error(codes.Internal,sysError)
-	}
-	dbReplies,err:=dao.GetRepliesByUserId(rr.UserId,int(rr.Offset),int(rr.Limit))
+	dbReplies,totalCount,err:=dao.GetRepliesByUserId(rr.UserId,rr.Page,rr.PageSize)
 	if err!=nil{
 		logs.Error(fmt.Sprintf("get replies by user_id failed: %+v",err))
 		return nil,err
@@ -380,23 +324,32 @@ func (p *Post) GetRepliesByUserId(ctx context.Context, rr *pb.RepliesByUserIdReq
 	for _,v:=range dbReplies{
 		replies=append(replies,&pb.Reply{
 			Id:v.Id,
-			UserId:v.UserId,
-			CommentId:v.CommentId,
-			PostId:v.PostId,
+			UserId:(v.UserId),
+			CommentId:int64(v.CommentId),
+			PostId:int64(v.PostId),
 			Content:v.Content,
 			CreateTime:v.CreateTime,
-			ParentId:v.ParentId,
-			Floor:v.Floor,
+			ParentId:int64(v.ParentId),
+			Floor:int64(v.Floor),
 			Status:pb.Status(v.Status),
 		})
 	}
 	return &pb.RepliesByUserIdResponse{
 		Replies:replies,
-		TotalCount:int32(totalCount),
+		TotalCount:int64(totalCount),
 	}, nil
 }
 
 func NewPostService(addr string,port int){
+	service:=micro.NewService(
+		micro.Name("post"),
+	)
+	service.Init()
+	pb.RegisterPostServiceHandler(service.Server(),new(Post))
+	if err:=service.Run();err!=nil{
+		
+	}
+	
 	lis,err:=net.Listen("tcp",fmt.Sprintf("%s:%d",addr,port))
 	if err!=nil{
 		logs.Error(fmt.Sprintf("post service failed to listen: %+v",err))
