ff6e712b5020ae3b3c0e37b7fd7454952bfb9501
liuwei1 <liuwei1@domob-inc.cn>
2021-02-26 19:26:45 +0800

    update

---
+++ gateway/middleware/tracing.go
@@ -0,0 +1,29 @@
+package middleware
+
+import (
+	"context"
+	"net/http"
+
+	"google.golang.org/grpc/metadata"
+)
+
+func injectHeadersIntoMetadata(ctx context.Context, req *http.Request) metadata.MD {
+	var (
+		otHeaders = []string{
+			"x-request-id",
+			"x-b3-traceid",
+			"x-b3-spanid",
+			"x-b3-parentspanid",
+			"x-b3-sampled",
+			"x-b3-flags",
+			"x-ot-span-context"}
+	)
+	var pairs []string
+
+	for _, h := range otHeaders {
+		if v := req.Header.Get(h); len(v) > 0 {
+			pairs = append(pairs, h, v)
+		}
+	}
+	return metadata.Pairs(pairs...)
+}
